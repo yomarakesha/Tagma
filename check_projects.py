2beb9f184b95e6f8d736cdfe6b5c0996f29aed9f
Yomarakesha <yomarakesha@gmail.com>
2025-06-28 13:47:20 +0500

    Удалены лишние файлы и папки из репозитория

--- check_projects.py
+++
@@ -1,8 +0,0 @@
-from app import create_app, db
-from app.models.project import Project
-
-app = create_app()
-with app.app_context():
-    projects = Project.query.all()
-    for p in projects:
-        print(f"ID: {p.id}, Title: {p.title_en}")
